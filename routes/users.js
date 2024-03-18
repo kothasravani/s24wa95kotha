949ce9d8289222caa9fc365261ffa3f3ed40e9ab
kothasravani <112386396+kothasravani@users.noreply.github.com>
2024-03-18 13:27:47 -0500

    users

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
+var product = 1;
+var factor = 1;
+
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  factor++;
+  product *= factor;
+
+  res.send(`Product is: ${product}`);
 });
 
 module.exports = router;
